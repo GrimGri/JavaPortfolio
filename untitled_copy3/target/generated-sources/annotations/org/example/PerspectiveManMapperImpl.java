7948e7824c1ca923734aa5a3b260d015a98247c9
GrimGri <uhb6390@yandex.ru>
2025-06-15 23:01:46 +0500

    update new with db no lomdock Main new no file

--- untitled_copy3/target/generated-sources/annotations/org/example/PerspectiveManMapperImpl.java
+++ untitled_copy3/target/generated-sources/annotations/org/example/PerspectiveManMapperImpl.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-15T23:00:24+0500",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.7 (Oracle Corporation)"
+    date = "2025-06-15T23:01:23+0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 24.0.1 (Oracle Corporation)"
 )
 @Component
 public class PerspectiveManMapperImpl implements PerspectiveManMapper {
